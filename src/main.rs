82b407b172a3af124e1a816ce883ef5bcb12d576
haxul <s.s.starodubov@yandex.ru>
2021-07-27 11:55:55 +0400

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+use std::collections::HashMap;
+
+
+#[tokio::main]
+async fn main() {
+    let result = request(String::from("java")).await;
+
+}
+
+async fn request(qr: String) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let url = &format!("https://api.hh.ru/vacancies?{}", qr);
+    let res = client.get(url)
+        .header("User-Agent", "vsearch")
+        .send()
+        .await?;
+
+    let body: String = res.text().await?;
+    println!("Body:\n{}", body);
+
+    Ok(body)
 }
